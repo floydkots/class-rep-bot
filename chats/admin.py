bd07ba374c26a612359d9bf89ede320c5d81be64
Floyd Kots <floydkotohoyoh@gmail.com>
2017-03-27 13:49:32 +0300

    feat: Registered student chat with the admin

--- chats/admin.py
+++ chats/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import StudentChat, LecturerChat
 
-# Register your models here.
+
+class StudentChatAdmin(admin.ModelAdmin):
+    list_display = ('student', 'chat_id', 'mobile_code', 'email_code', 'mobile_verified', 'email_verified', 'latest_chat')
+
+
+admin.site.register(StudentChat, StudentChatAdmin)
+
+
+
